842bad206e40971cf57f4809a6c1f44cd94ec0d1
Harrison Liddiard <omniaura5@gmail.com>
2019-09-26 03:53:24 +0100

    null checks, LED brightness, use empty lines instead of 'NULL' for skipped LEDs

--- metar.py
+++ metar.py
@@ -9,6 +9,7 @@ pixels = neopixel.NeoPixel(
     constants.LED_PIN,
     constants.LED_COUNT,
     pixel_order=constants.LED_ORDER,
+    brightness=constants.LED_BRIGHTNESS,
     auto_write=False
 )
 
@@ -21,15 +22,16 @@ airport_conditions = get_metars(airports)
 
 # Setting LED colors based on weather conditions
 for i, airport in enumerate(airports):
-    # Skip NULL entries
-    if airport == "NULL":
+    # Skip empty entries
+    if not airport:
         continue
     
-    flight_category = airport_conditions.get(airport)["flight_category"]
+    flight_category = airport_conditions.get(airport, {}).get("flight_category")
     color = constants.FLIGHT_CATEGORY_TO_COLOR_MAP.get(flight_category, constants.COLOR_OFF)
     
     print()
-    print("Setting LED " + str(i) + " for " + airport + " to " + flight_category + " " + str(color))
+    if (flight_category):
+        print("Setting LED " + str(i) + " for " + airport + " to " + flight_category + " " + str(color))
     pixels[i] = color
 
 # Update actual LEDs all at once
