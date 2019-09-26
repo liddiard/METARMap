fc4a0fe4e7685560175e9aad1d4f5c6ac98f25c5
Harrison Liddiard <hliddiard@paypal.com>
2019-09-25 19:35:17 -0700

    update colors for RGB order

--- constants.py
+++ constants.py
@@ -7,10 +7,10 @@ LED_PIN			= board.D18				# GPIO pin connected to the pixels (18 is PCM).
 LED_BRIGHTNESS	= 0.5					# Float from 0.0 (min) to 1.0 (max)
 LED_ORDER		= neopixel.RGB			# Strip type and colour ordering
 
-COLOR_VFR		= (255,0,0)				# Green
+COLOR_VFR		= (0,255,0)				# Green
 COLOR_MVFR		= (0,0,255)				# Blue
-COLOR_IFR		= (0,255,0)				# Red
-COLOR_LIFR		= (0,125,125)			# Magenta
+COLOR_IFR		= (255,0,0)				# Red
+COLOR_LIFR		= (125,0,125)			# Magenta
 COLOR_OFF		= (0,0,0)				# Off
 
 FLIGHT_CATEGORY_TO_COLOR_MAP = {
